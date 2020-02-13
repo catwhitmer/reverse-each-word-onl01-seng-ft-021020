9bd5edc404492fc35a539c290de64d9307e13bf3
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-13 13:39:54 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,22 @@
+#reverse method 
+#string argument
+#returns sentence with word reversed
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  new_array = []
+  array.each do|string|
+    new_array << string.reverse
+  end
+  return_array.join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split(" ") 
+  new_array = []
+  array.collect do |string| 
+    new_array << string.reverse 
+  end
+  new_array.join(" ")
+end
+
